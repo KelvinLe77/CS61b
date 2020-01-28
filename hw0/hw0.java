258e855a9df82693022d6fdd1c9f50c997970eff
Kelvin Le <kelvinle7777@berkeley.edu>
2020-01-28 14:03:34 -0800

    .-.

---
+++ hw0/hw0.java
@@ -0,0 +1,45 @@
+public class hw0 {
+
+  public static int max(int[] a) {
+    int max = a[0];
+    int pointer = 0;
+    for (int x = 0; pointer < a.length; pointer += 1) {
+      if (a[pointer] > max) {
+        max = a[pointer];
+      }
+    }
+    return max;
+  }
+
+  public static boolean threeSum(int[] a) {
+    for (int x = 0; x < a.length; x += 1) {
+      for (int y = 0; y < a.length; y += 1) {
+        for (int z = 0; z < a.length; z += 1) {
+          if (a[x] + a[y] + a[z] == 0) {
+            return true;
+          }
+          else {
+            return false;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  public static boolean threesumDistinct(int[] a) {
+    for (int x = 0; x < a.length; x += 1) {
+      for (int y = 0; y < a.length; y += 1) {
+        for (int z = 0; z < a.length; z += 1) {
+          if (a[x] + a[y] + a[z] == 0 && x != y && y != z && x != z) {
+            return true;
+          }
+          else {
+            return false;
+          }
+        }
+      }
+    }
+    return false;
+  }
+}
