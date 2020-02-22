0de4d5e23e0038bbaa8b7426cdecffdb72ac0c58
Kelvin Le <kelvinle7777@berkeley.edu>
2020-02-21 19:16:16 -0800

    add

--- hw3/WeirdList/WeirdListClient.java
+++ hw3/WeirdList/WeirdListClient.java
@@ -3,7 +3,7 @@ class WeirdListClient {
 
     /** Return the result of adding N to each element of L. */
     static WeirdList add(WeirdList L, int n) {
-        return null; // TODO: REPLACE THIS LINE
+        return L.map((int x) -> x + n) ; // TODO: REPLACE THIS LINE
     }
 
     /** Return the sum of all the elements in L. */
