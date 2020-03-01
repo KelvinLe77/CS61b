58549ec51d409f99c51a6bb4499e2ad4ad772258
Kelvin Le <kelvinle7777@berkeley.edu>
2020-02-29 20:49:00 -0800

    oopsies

--- lab6/enigma/PermutationTest.java
+++ lab6/enigma/PermutationTest.java
@@ -134,7 +134,7 @@ public abstract class PermutationTest {
         assertEquals(0, p.permute(3));
         assertEquals(5, p.permute(5));
         assertEquals(2, p.permute(8));
-        assertEquals(5, p.permute(-1));
+        assertEquals(6, p.permute(-1));
         assertEquals(3, p.permute(-5));
 
     }
