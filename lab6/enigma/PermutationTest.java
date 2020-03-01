36acbce330eec7ed2e92ae84b0db785484f99132
Kelvin Le <kelvinle7777@berkeley.edu>
2020-02-29 19:04:10 -0800

    modded for permute(int)

--- lab6/enigma/PermutationTest.java
+++ lab6/enigma/PermutationTest.java
@@ -129,10 +129,13 @@ public abstract class PermutationTest {
 
     @Test
     public void testPermuteInt() {
+
         Permutation p = getNewPermutation("(ABCD)", getNewAlphabet("ABCDEFG"));
-        assertEquals(3, p.permute(2));
-        assertEquals(0, p.permute(3));
-        assertEquals(5, p.permute(5));
+        assertEquals(3, p.permute(2 % p.alphabet().size()));
+        assertEquals(0, p.permute(3 % p.alphabet().size()));
+        assertEquals(5, p.permute(5 % p.alphabet().size()));
+        assertEquals(2, p.permute(27 % p.alphabet().size()));
+
     }
 
     @Test
