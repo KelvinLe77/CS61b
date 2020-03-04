3e1681f994de8de41788a716a456225dcbe0ec7c
Kelvin Le <kelvinle7777@berkeley.edu>
2020-03-03 16:37:28 -0800

    alpha

--- proj1/enigma/Alphabet.java
+++ proj1/enigma/Alphabet.java
@@ -1,5 +1,9 @@
 package enigma;
 
+import javax.swing.text.AttributeSet;
+import java.util.ArrayList;
+import java.util.HashSet;
+
 /** An alphabet of encodable characters.  Provides a mapping from characters
  *  to and from indices into the alphabet.
  *  @author
@@ -10,6 +14,19 @@ class Alphabet {
      *  K (numbering from 0). No character may be duplicated. */
     Alphabet(String chars) {
         // FIXME
+        HashSet<Character> charSet = new HashSet<>();
+        int charsLen = chars.length();
+        validAlpha = new Character[charsLen];
+        for (int i = 0; i < charsLen; i += 1) {
+            charSet.add(chars.charAt(i));
+        }
+        if (charSet.size() != charsLen) {
+            throw new EnigmaException("Alphabet contains duplicate characters");
+        } else {
+            for (int i = 0; i < charsLen; i += 1) {
+                validAlpha[i] = chars.charAt(i);
+            }
+        }
     }
 
     /** A default alphabet of all upper-case characters. */
@@ -19,24 +36,44 @@ class Alphabet {
 
     /** Returns the size of the alphabet. */
     int size() {
-        return 26; // FIXME
+        return validAlpha.length; // FIXME
     }
 
     /** Returns true if CH is in this alphabet. */
     boolean contains(char ch) {
-        return 'A' <= ch && ch <= 'Z'; // FIXME
+        for (Character character : validAlpha) {
+            if (character.equals(ch)) {
+                return true;
+            }
+        }
+        return false; // FIXME
     }
 
     /** Returns character number INDEX in the alphabet, where
      *  0 <= INDEX < size(). */
     char toChar(int index) {
-        return (char) ('A' + index); // FIXME
+        if (index < 0 || index >= validAlpha.length) {
+            throw new EnigmaException("invalid index");
+        }
+        return validAlpha[index]; // FIXME
     }
 
     /** Returns the index of character CH which must be in
      *  the alphabet. This is the inverse of toChar(). */
     int toInt(char ch) {
-        return ch - 'A'; // FIXME
+        for (int i = 0; i < validAlpha.length; i += 1) {
+            if (validAlpha[i].equals(ch)) {
+                return i;
+            }
+        }
+        throw new EnigmaException("Character not contained in Alphabet");
+        // FIXME
     }
 
+    /**
+     * A list of characters in alphabet.
+     */
+    private Character[] validAlpha;
 }
+
+
